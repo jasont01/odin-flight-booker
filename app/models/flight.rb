5eb58b0797b6383623255ac27a899a7b09b9d55b
Jason Thompson <jason@jasont.us>
2020-06-09 15:43:43 -0400

    add search form for flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,6 +3,10 @@ class Flight < ApplicationRecord
   belongs_to :to_airport, class_name: "Airport", foreign_key: "dest"
 
   def flight_date_formatted
+    date.strftime("%Y-%m-%d")
+  end
+
+  def flight_date_formatted_user
     date.strftime("%m/%d/%Y")
   end
 end
