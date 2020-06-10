712bdc03e329e5f991744f0c3b5c04eb2bdf70a1
Jason Thompson <jason@jasont.us>
2020-06-10 12:39:48 -0400

    wip

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,33 +1,33 @@
 class BookingsController < ApplicationController
 
   def new
-    @num_passengers = params[:tickets] || 2
-    @booking = Booking.new(:flight_id => params[:flight])
-    @booking.passengers.new
+    @flight = Flight.find_by_id(params[:flight])
+    @booking = Booking.new
+
+    @num_passengers = params[:tickets].to_i
+    @num_passengers.times { @booking.passengers.build }
   end
   
   def create
-    redirect_to @booking
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      flash[:notice] = "Booking Confirmed"
+      redirect_to @booking
+    else
+      flash.now[:alert] = "Something went wrong."
+      render 'new'
+    end
   end
 
   def show
-    @booking = Booking.last #testing
-    
-    #@depature_time = @booking.flight.date.in_time_zone(@booking.flight.from_airport.timezone).strftime("%a, %d %b %Y %I:%M %p")
-    @depature_time = format_time(@booking.flight.date, @booking.flight.from_airport.timezone)
-    
-    # Arrival Time - add flight duration(in seconds) to depature time
-    @arrival_time = @booking.flight.date + (@booking.flight.duration * 60)
-    #@arrival_time = @arrival_time.in_time_zone(@booking.flight.to_airport.timezone).strftime("%a, %d %b %Y %I:%M %p")
-
-    @arrival_time = format_time(@arrival_time, @booking.flight.to_airport.timezone)
-    
-    #@flight_time_mins = @booking.flight.duration % 60
-    #@flight_time_hrs = (@booking.flight.duration - @flight_time_mins) / 60
-    @flight_time = "#{(@booking.flight.duration - (@booking.flight.duration % 60)) / 60} hrs #{@booking.flight.duration % 60} mins"
+    @booking = Booking.find(params[:id])
   end
 
-  def format_time(time, timezone)
-    time.in_time_zone(timezone).strftime("%a, %d %b %Y %I:%M %p")
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:first_name, :last_name, :email])
   end
+
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,11 +4,31 @@ class Flight < ApplicationRecord
   has_many :bookings
   has_many :passengers, through: :bookings
 
+  def depature_time
+    format_time(date, from_airport.timezone)
+  end
+
+  def arrival_time
+    # Arrival Time - add flight duration(in seconds) to depature time
+    format_time(date + (duration * 60), to_airport.timezone)
+  end
+  
+  def flight_time
+    "#{(duration - (duration % 60)) / 60} hrs #{duration % 60} mins"
+  end
+
   def flight_date_formatted
     date.strftime("%Y-%m-%d")
   end
 
-  def flight_date_formatted_user
+  def flight_date_formatted_dropdown
     date.strftime("%m/%d/%Y")
   end
+
+  private
+
+  def format_time(time, timezone)
+    time.in_time_zone(timezone).strftime("%a, %d %b %Y %I:%M %p")
+  end
+
 end
