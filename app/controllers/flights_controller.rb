01c5feb1834e1ab20b7a0e771cf320b5efcf5856
Jason Thompson <jason@jasont.us>
2020-06-12 01:19:14 -0400

    refactor SQL query for heroku deployment

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -11,9 +11,14 @@ class FlightsController < ApplicationController
   def search_flights
     if params[:origin] == params[:destination]
       flash.now[:alert] = "Please choose different departure and arrival cities."
+    elsif 
+      params[:departure_date].empty?
+      @results = Flight.where("origin_id = ? AND destination_id = ?", params[:origin], params[:destination])
     else
-      date = Date.parse(params[:departure_date]) if !params[:departure_date].empty?
-      @results = Flight.where("origin_id = ? AND destination_id = ? AND departure_date LIKE ?", params[:origin], params[:destination], "#{date}%")
+      date = Date.parse(params[:departure_date])
+      date_begin = date.beginning_of_day
+      date_end = date.end_of_day
+      @results = Flight.where("origin_id = ? AND destination_id = ? AND departure_date > ? AND departure_date < ?", params[:origin], params[:destination], date_begin, date_end)
     end
   end
 
